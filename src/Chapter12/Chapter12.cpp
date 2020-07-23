b888563f2d863c36d4e0e3d770b95054d723a5b5
xiaoyi <linyihuang@hitech.com>
2020-07-23 11:42:34 +0800

    linxiaoyi_20200723

--- src/Chapter12/Chapter12.cpp
+++ src/Chapter12/Chapter12.cpp
@@ -91,10 +91,14 @@ int main()
 
 
 
+
+/*
+//Task2
 #include <cstring>
+#include<string>
 #include <iostream>
 using namespace std;
-//Task2
+
 class Str
 {
 private:
@@ -107,9 +111,19 @@ public:
 	Str(const Str& st);
 	~Str();
 
+	Str& operator=(const char* ch);
+	Str& operator=(const Str& st);
+
+	void StringUp();
+	void StringLow();
+	int has(char ch);
+	bool operator==(Str& str);
+
 	friend ostream& operator<<(ostream& os, const Str& st);
 	friend istream& operator>>(istream& is, Str& st);
+	friend Str operator+(Str& ch, Str& st);
 	friend Str operator+(char* ch, Str& st);
+	
 };
 
 Str::Str()
@@ -123,7 +137,7 @@ Str::Str(const char* s)
 {
 	len = strlen(s);
 	str = new char[len + 1];
-	strcpy(str, s);
+	strcpy_s(str, len +1,s);
 }
 
 Str::~Str()
@@ -135,14 +149,75 @@ Str::Str(const Str& st)
 {
 	len = st.len;
 	str = new char[len + 1];	
-	strcpy(str, st.str);
+	strcpy_s(str,len+1, st.str);
+}
+
+Str& Str::operator=(const char* ch)
+{
+	delete[] str;
+	len = strlen(ch);
+	str = new char[len + 1];
+	strcpy_s(str, len + 1, ch);
+	return *this;
+}
+
+Str& Str::operator=(const Str& st)
+{
+	if (this == &st )
+	{
+		*this;
+	}
+	delete[] str;
+	len = st.len;
+	str = new char[len + 1];
+	strcpy_s(str, len + 1, st.str);
+	return *this;
+}
+
+void Str::StringUp()
+{
+	const int delta = 'A' - 'a';
+	for (int i = 0; i < len; i++)
+	{
+		if (str[i] >= 'a'&&str[i] <= 'z')
+		{
+			str[i] = str[i] + delta;
+		}
+	}
+}
+
+void Str::StringLow()
+{
+	const int delta = 'a' - 'A';
+	for (int i = 0; i < len; i++)
+	{
+		if (str[i] >= 'A'&& str[i] <= 'Z')
+		{
+			str[i] = str[i] + delta;
+		}
+	}
+}
+
+int Str::has(char ch)
+{
+	int count = 0;
+	for (int i = 0; i < len; i++)
+	{
+		if (str[i] == ch)
+			count++;
+	}
+	return count;
+}
+
+bool Str::operator==(Str& st)
+{
+	return strcmp(str, st.str) == 0;
 }
 
 
 ostream& operator<<(ostream& os, const Str& st)
 {
-	os << "Len: " << st.len << endl;
-	os << "Str: " << st.str << endl;
+	os << st.str << endl;
 	return os;
 }
 
@@ -153,7 +228,7 @@ istream& operator>>(istream& is, Str& st)
 	is.get(temp, Str::CINLIM);
 	if (is)
 	{
-		st.str = temp;
+		st = temp;
 	}
 	while (is && is.get() != '\n')
 	{
@@ -162,23 +237,65 @@ istream& operator>>(istream& is, Str& st)
 	return is;
 }
 
+Str operator+(Str& ch, Str& st)
+{
+	Str temp;
+	temp.len = ch.len + st.len;
+	temp.str = new char[temp.len + 1];
+	strcpy_s(temp.str,temp.len+1, ch.str);
+	strcat(temp.str, st.str);
+	return temp;
+}
+
 Str operator+(char* ch, Str& st)
 {
 	Str temp;
 	temp.len = strlen(ch) + st.len;
 	temp.str = new char[temp.len + 1];
-	strcpy(temp.str, ch);
-	strcpy(temp.str, st.str);
+	strcpy_s(temp.str,temp.len+1, ch);
+	strcat(temp.str, st.str);
 	return temp;
 }
-
 int main()
 {
 	Str s1("and I am a C++ student.");
 	Str s2 = "Please enter your name: ";
 	Str s3;
-	
 	cout << s2;
 	cin >> s3;
+
 	s2 = "My names is " + s3;
-}
+	cout << s2 << endl;
+	s2 = s2 + s1;
+	s2.StringUp();
+	cout << "The string\n" << s2 << "\ncontains " << s2.has('A') << "'A' characters in it" << endl;
+
+	Str rgb[3] = { Str(s1), Str(" green"),Str("blue") };
+	cout << "Enter the name of a primary color for mixing light: ";
+	Str ans;
+	bool success = false;
+	while (cin >> ans)
+	{
+		ans.StringLow();
+		for (int i = 0; i < 3; i++)
+		{
+			if (ans == rgb[i])
+			{
+				cout << "That's right!" << endl;
+				success = true;
+				break;
+			}
+		}
+		if (success)
+			break;
+		else
+		{
+			cout << "Try again!" << endl;
+
+		}
+	}
+	cout << "Bye" << endl;
+	return 0;
+ }
+
+ */
