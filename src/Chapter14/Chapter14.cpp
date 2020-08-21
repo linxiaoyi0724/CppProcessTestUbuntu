abe26ce233bd373f249a72f250e5c65ae74b3398
xiaoyi <linyihuang@hitech.com>
2020-08-21 14:36:41 +0800

    linxiaoyi_20200821

--- src/Chapter14/Chapter14.cpp
+++ src/Chapter14/Chapter14.cpp
@@ -29,4 +29,35 @@ public:
 	Wine(const char* l, int y);
 	void GetBottles();
 	string& Label();
+	int Sum();
 };
+
+
+Wine::Wine(const char* l, int y, const int yr[], const int bot[])
+{
+
+}
+
+int main()
+{
+	cout << "Enter name of wine: ";
+	char lab[50];
+	cin.getline(lab, 50);
+	cout << "Enter number of year: ";
+	int yrs;
+	cin >> yrs;
+
+	Wine holding(lab, yrs);
+	holding.GetBottles();
+	holding.Show();
+
+	const int YRS = 3;
+	int y[YRS] = { 1993, 1995, 1998 };
+	int b[YRS] = { 48, 60, 72 };
+
+	Wine more("Gushing Grape Red", YRS, y, b);
+	more.Show();
+	cout << "Total bottles for " << more.Label() << ": " << more.sum() << endl;
+	cout << "Bye" << endl;
+	return 0;
+}
