25eb43df5289cbc321391a433ba74a5f6380363f
xiaoyi <linyihuang@hitech.com>
2020-08-24 13:54:07 +0800

    linxiaoyi_20200824

--- src/Chapter14/Chapter14.cpp
+++ src/Chapter14/Chapter14.cpp
@@ -3,8 +3,6 @@
 #include <valarray>
 #include <string>
 using namespace std;
-typedef std::valarray<int> ArrayInt;
-typedef Paris<ArrayInt, ArrayInt> pariArray;
 
 template<typename T1, typename T2>
 class Paris
@@ -15,27 +13,91 @@ private:
 public:
 	Paris(const T1& aval, const T2& bval) :a(aval),b(bval){}
 	Paris(){}
+	void Set(T1& yr, T2& bot);
+	int Sum();
+	void Show()const;
 };
+
+typedef std::valarray<int> ArrayInt;
+typedef Paris<ArrayInt, ArrayInt> pariArray;
 
 class Wine
 {
 private:
 	string name;
 	pariArray vintageAndCounts;
-	int storageYear;
+	int kindYear;
 
 public:
 	Wine(const char* l, int y, const int yr[], const int bot[]);
 	Wine(const char* l, int y);
 	void GetBottles();
 	string& Label();
-	int Sum();
+	void Show()const;
+	int sum();
+	
 };
+template<typename T1, typename T2>
+void Paris<T1,T2>::Set(T1& yr, T2& bot)
+{
+	a = yr;
+	b = bot;
+}
 
+template<typename T1, typename T2>
+int Paris<T1, T2>::Sum()
+{
+	return b.sum();
+}
 
-Wine::Wine(const char* l, int y, const int yr[], const int bot[])
+template<typename T1, typename T2>
+void Paris<T1, T2>::Show()const
+{
+	for (int i = 0; i < a.size(); i++)
+	{
+		cout << a[i] << ": " << b[i] << endl;
+	}
+}
+
+Wine::Wine(const char* l, int y, const int yr[], const int bot[]) :name(l),kindYear(y)
+{
+	vintageAndCounts.Set(ArrayInt(yr, y), ArrayInt(bot, y));
+}
+
+Wine::Wine(const char* l, int y) : name(l), kindYear(y)
 {
 
+}
+
+void Wine::GetBottles()
+{
+	ArrayInt yr(kindYear), bot(kindYear);
+	cout << "Enter " << name << "data for " << kindYear << "year(s): " << endl;
+	for (int i = 0; i < kindYear; i++)
+	{
+		cout << "Enter year: ";
+		cin >> yr[i];
+		cout << "Enter bottles for that year: ";
+		cin >> bot[i];
+	}
+	vintageAndCounts.Set(yr, bot);
+}
+
+string& Wine::Label()
+{
+	return name;
+}
+
+void Wine::Show()const
+{
+	cout << "Wine: " << name << endl;
+	cout << "\t\t year \t\t Bottles " << endl;
+	vintageAndCounts.Show();
+}
+
+int Wine::sum()
+{
+	return vintageAndCounts.Sum();
 }
 
 int main()
